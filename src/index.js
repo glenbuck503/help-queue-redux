20df201d86870af3b99dc945104b4d4079c9c987
glenbuck503 <glenbuck503@gmail.com>
2021-07-12 15:36:24 -0700

    add firestore

--- src/index.js
+++ src/index.js
@@ -5,22 +5,31 @@ import reportWebVitals from './reportWebVitals';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-// import reducer from './reducers/ticket-list-reducer';
 import rootReducer from './reducers/index';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+import firebase from "./firebase";
 
 
 const store = createStore(rootReducer);
 
-store.subscribe(() =>
-  console.log(store.getState())
-);
+const rrfProps = {
+  firebase,
+  config: {
+        userProfile: "users"
+    },
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById('root')
-);
+)
 
 
 reportWebVitals();
